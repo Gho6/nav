b28bcc6207b66e32b86491519aac17dd7cb03fab
Gho6 <136088471+Gho6@users.noreply.github.com>
2026-02-07 04:10:05 +0000

    Deploying to gh-pages from @ Gho6/nav@a235268789e63d2398e25c5518e52987d7db4aa4 🚀

--- chunk-BBTE6E2A.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-LE7U3YG7.js";import{Ab as e,sb as r}from"./chunk-6U6E2YVF.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
