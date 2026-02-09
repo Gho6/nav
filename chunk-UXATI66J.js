3f781ff17b513e5b091ea938abcfcf1dcca7e5d2
Gho6 <136088471+Gho6@users.noreply.github.com>
2026-02-09 04:19:00 +0000

    Deploying to gh-pages from @ Gho6/nav@ea88a8051b3bec6e7975a7b6ec23ebae4e2ed1ec 🚀

--- chunk-UXATI66J.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-L3B3N7LR.js";import{Ab as e,sb as r}from"./chunk-NEAOE2CS.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
